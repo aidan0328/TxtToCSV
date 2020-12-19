3c3e0ec25691330024ca4827ff3f706942f70019
aidan.lu <aidan0328@gmail.com>
2020-12-20 02:52:01 +0800

    加入進度條；能取消轉換。
    1. 透過 _BW_ProgressChanged() 與 _BW_RunWorkerCompleted() 更新進度條。

--- TxtToCSV/Form1.Designer.cs
+++ TxtToCSV/Form1.Designer.cs
@@ -27,13 +27,16 @@ namespace TxtToCSV {
             this.btnAddTxt = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.btnConvertToCsv = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCancle = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnAddTxt
             // 
             this.btnAddTxt.Location = new System.Drawing.Point(12, 12);
             this.btnAddTxt.Name = "btnAddTxt";
-            this.btnAddTxt.Size = new System.Drawing.Size(81, 42);
+            this.btnAddTxt.Size = new System.Drawing.Size(81, 30);
             this.btnAddTxt.TabIndex = 0;
             this.btnAddTxt.Text = "加入 Txt";
             this.btnAddTxt.UseVisualStyleBackColor = true;
@@ -50,25 +53,56 @@ namespace TxtToCSV {
             // 
             // btnConvertToCsv
             // 
-            this.btnConvertToCsv.Location = new System.Drawing.Point(12, 60);
+            this.btnConvertToCsv.Location = new System.Drawing.Point(12, 53);
             this.btnConvertToCsv.Name = "btnConvertToCsv";
-            this.btnConvertToCsv.Size = new System.Drawing.Size(81, 42);
+            this.btnConvertToCsv.Size = new System.Drawing.Size(81, 30);
             this.btnConvertToCsv.TabIndex = 2;
             this.btnConvertToCsv.Text = "轉換為 CSV";
             this.btnConvertToCsv.UseVisualStyleBackColor = true;
             this.btnConvertToCsv.Click += new System.EventHandler(this.btnConvertToCsv_Click);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(112, 106);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(411, 18);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(529, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "0%";
+            // 
+            // btnCancle
+            // 
+            this.btnCancle.Location = new System.Drawing.Point(12, 94);
+            this.btnCancle.Name = "btnCancle";
+            this.btnCancle.Size = new System.Drawing.Size(81, 30);
+            this.btnCancle.TabIndex = 5;
+            this.btnCancle.Text = "取消";
+            this.btnCancle.UseVisualStyleBackColor = true;
+            this.btnCancle.Visible = false;
+            this.btnCancle.Click += new System.EventHandler(this.btnCancle_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(568, 136);
+            this.Controls.Add(this.btnCancle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.btnConvertToCsv);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.btnAddTxt);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,6 +110,9 @@ namespace TxtToCSV {
         private System.Windows.Forms.Button btnAddTxt;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button btnConvertToCsv;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCancle;
     }
 }
 
--- TxtToCSV/Form1.cs
+++ TxtToCSV/Form1.cs
@@ -11,15 +11,66 @@ using System.IO;
 
 namespace TxtToCSV {
     public partial class Form1 : Form {
-        //非同步作業
-        void DoWork(object sender, DoWorkEventArgs e) {
+        BackgroundWorker backgroundWorker1 = new BackgroundWorker();
 
-            //呼叫自訂函式
-            List<String> files = (List<String>)e.Argument;
-            OpenAndConverToCSV(files);
+        //在這麼事件裡，可以存取表單的資訊
+        void _BW_ProgressChanged(object sender, ProgressChangedEventArgs e) {
+            //若是已經取消背景作業，就不去更新表單資訊，
+            //否則會引發 [引動過程的目標傳回例外狀況]。
+            if (((BackgroundWorker)sender).CancellationPending == false) {
+                progressBar1.Value = e.ProgressPercentage;
+                int percentage = (progressBar1.Value * 100 / progressBar1.Maximum);
+                label1.Text = $"{percentage}%";
+
+            }
         }
 
-        static void OpenAndConverToCSV(List<String> files) {
+        //非同步作業已完成
+        void _BW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            if (e.Cancelled) // 非同步作業已被取消...
+            {
+                btnAddTxt.Enabled = true;
+                btnConvertToCsv.Enabled = true;
+            } else // 非同步作業順利完成
+            {
+                btnAddTxt.Enabled = true;
+                btnConvertToCsv.Enabled = true;
+                progressBar1.Value = progressBar1.Maximum;
+                label1.Text = "100%";
+                btnCancle.Visible = false;
+            }
+            btnCancle.Visible = false;
+        }
+
+        //非同步作業
+        void DoWork(object sender, DoWorkEventArgs e) {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            //設定 BW 物件可以更新表單資訊報告進度
+            worker.WorkerReportsProgress = true;
+
+            //設定是否支援非同步作業取消
+            worker.WorkerSupportsCancellation = true;
+            // If the operation was canceled by the user,
+            // set the DoWorkEventArgs.Cancel property to true.
+            if (worker.CancellationPending) {
+                e.Cancel = true;
+            }
+
+            //繫結 ProgressChanged 事件，當 WorkerReportsProgress 屬性設為 true ，
+            //會觸發該事件，同時在這麼函式下，可以更新表單資訊
+            worker.ProgressChanged += new ProgressChangedEventHandler(_BW_ProgressChanged);
+
+            //當背景作業已完成時、取消、例外發生時皆會觸發該事件
+            worker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(_BW_RunWorkerCompleted);
+
+            //呼叫自訂函式
+            OpenAndConverToCSV(worker, e);
+        }
+
+        static void OpenAndConverToCSV(BackgroundWorker worker, DoWorkEventArgs e) {
+            List<String> files = (List<String>)e.Argument;
+            int percentProgress = 0;
+
             foreach (string path in files) {
                 if (File.Exists(path) == true) {
                     // Open the file to read from.
@@ -27,6 +78,7 @@ namespace TxtToCSV {
                         if (File.Exists(path + ".csv") == true) {
                             File.Delete(path + ".csv");
                         }
+
                         using (StreamWriter sw = File.CreateText(path + ".csv")) {
 
                             string s;
@@ -36,8 +88,24 @@ namespace TxtToCSV {
                                 foreach (String item in str) {
                                     newStr += $"{item},";
                                 }
-                                Console.WriteLine(newStr);
                                 sw.WriteLine(newStr);
+
+                                //判斷使用者是否已經取消背景作業
+                                if (worker.CancellationPending == true) {
+
+                                    //設定取消事件
+                                    e.Cancel = true;
+
+                                    // 離開
+                                    return;
+                                }
+
+                                ++percentProgress;
+                                if ((percentProgress % 100) == 0) {
+                                    // 呼叫這個方法，會觸發 ProgressChanged 事件    
+                                    // 這邊會另外開一條執行緒去觸發事件 (_BW_ProgressChanged)。
+                                    worker.ReportProgress(percentProgress);
+                                }
                             }
                         }
                     }
@@ -55,8 +123,6 @@ namespace TxtToCSV {
             openFileDialog1.Filter = "TXT files (*.txt)|*.txt";
             if (openFileDialog1.ShowDialog() == DialogResult.OK) {
                 String filePath = openFileDialog1.FileName;
-                //MessageBox.Show(openFileDialog1.FileName);
-
                 if (listBox1.Items.Contains(filePath) == false) {
                     listBox1.Items.Add(openFileDialog1.FileName);
                 }
@@ -69,17 +135,36 @@ namespace TxtToCSV {
             else {
                 btnAddTxt.Enabled = false;
                 btnConvertToCsv.Enabled = false;
+                btnCancle.Visible = true;
+                progressBar1.Value = 0;
 
+                // 將檔案的完整路徑加入倒 files
                 List<String> files = new List<String>();
                 foreach (String s in listBox1.Items) {
                     files.Add(s);
                 }
-                BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+
+                // 以行為單位，決定 progressBar1 的最大值
+                int Maximum = 0;
+
+                foreach (String s in files) {
+                    if (File.Exists(s) == true) {
+                        Maximum += File.ReadAllLines(s).Length;
+                    }
+                }
+                progressBar1.Maximum = Maximum;
+
+                // 採用非同步的 BackgroundWorker 進行轉換
+
                 backgroundWorker1.DoWork += DoWork;
                 backgroundWorker1.RunWorkerAsync(files);
+            }
+        }
 
-                btnAddTxt.Enabled = true;
-                btnConvertToCsv.Enabled = true;
+        private void btnCancle_Click(object sender, EventArgs e) {
+            // 先判斷非同步作業是否仍在執行中。
+            if (backgroundWorker1.IsBusy) {
+                backgroundWorker1.CancelAsync();
             }
         }
     }
